7f4505a92582a2c2a91f0a7c60ba3d62d6e73263
AstroMT <81778387+AstroMT@users.noreply.github.com>
2021-10-23 10:41:16 -0400

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,1 +1,18 @@
-//
+const firebaseConfig = {
+    apiKey: "AIzaSyBuwQqNN6b6tLtaygawyb6DpVf4wouImfk",
+    authDomain: "letschat-b90a7.firebaseapp.com",
+    databaseURL: "https://letschat-b90a7-default-rtdb.firebaseio.com",
+    projectId: "letschat-b90a7",
+    storageBucket: "letschat-b90a7.appspot.com",
+    messagingSenderId: "401013938739",
+    appId: "1:401013938739:web:8749396c98bd70b0fa6146"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+function addRoom() {
+    room_name = document.getElementById("room_name").value;
+    firebase.database().ref("/").child(room_name).update({
+        purpose: "adding room"
+    });
+}
